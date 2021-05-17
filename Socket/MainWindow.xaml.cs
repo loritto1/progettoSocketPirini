594b01b791dc4e28c17ac66b205e8866270643a6
Pirini <lorenzo.pirini.stud@ispascalcomandini.it>
2021-05-17 10:05:42 +0200

    fatto il primo punto

---
+++ Socket/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Aggiunta delle seguenti librerie
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace socket
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreaSocket_Click(object sender, RoutedEventArgs e)
+        {
+            IPEndPoint sourceSocket = new IPEndPoint(IPAddress.Parse("192.168.1.233"), 56000);
+            if (txtIP.Text.Length > 8 && txtIP.Text.Contains("."))
+            {
+                if (txtPort.Text != "")
+                {
+                    btnInvia.IsEnabled = true;
+                }
+            }
+
+            //btnInvia.IsEnabled = true;
+
+            Thread ricezione = new Thread(new ParameterizedThreadStart(SocketReceive));
+
+            ricezione.Start(sourceSocket);
+
+        }
+
+        private void btnInvia_Click(object sender, RoutedEventArgs e)
+        {
+            //aggiungere controlli sul contenuto delle textbox
+            string ipAddress = txtIP.Text;
+            int port = int.Parse(txtPort.Text);
+
+            SocketSend(IPAddress.Parse(ipAddress), port, txtMsg.Text);
+
+
+        }
+        //metodo per ricevere i messaggi
+        public async void SocketReceive(object socksource)
+        {
+            IPEndPoint ipendp = (IPEndPoint)socksource;
+
+            Socket t = new Socket(ipendp.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            t.Bind(ipendp);
+
+            Byte[] bytesRicevuti = new Byte[256];
+
+            string message;
+
+            int contaCaratteri = 0;
+
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    if(t.Available >0)
+                    {
+                        message = "";
+
+                        contaCaratteri = t.Receive(bytesRicevuti, bytesRicevuti.Length,0);
+                        message = message + Encoding.ASCII.GetString(bytesRicevuti, 0, contaCaratteri);
+
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+
+                            lblRicevi.Content = message;
+                        }));
+
+                    }
+
+                }
+
+            });
+
+        }
+        // metodo per iviare messaggi 
+        public void SocketSend(IPAddress dest, int destport, string message)
+        {
+            Byte[] byteInviati = Encoding.ASCII.GetBytes(message);
+
+            Socket s = new Socket(dest.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint remote_endpoint = new IPEndPoint(dest, destport);
+
+            s.SendTo(byteInviati, remote_endpoint);
+        }
+    
+    }
+}
